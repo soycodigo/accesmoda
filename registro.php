7c6d9640b7a247b034d1e0eac965ec9206ef8524
soycodigo <soycodigo@gmail.com>
2015-05-23 14:05:01 -0700

    subo la vista del catalogo

---
+++ registro.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html> 
+ <html>
+   <head>
+     <title>Baby Doll's que sorprenden</title>
+     <title>Grillas con Materialize</title>
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	 <meta name=viewport content="width=device-width, initial-scale=1">
+    	 <meta charset="utf-8">
+    	 <script src="js/jquery-2.1.4.js"></script>
+    	 <script src="js/materialize.js"></script>
+    	 <script src="js/mijs.js"></script>
+       <link rel="icon" type="image/png" href="img/favicon.png">
+    	 <link rel="stylesheet" type="text/css" href="css/materialize.css">
+    	 <link rel="stylesheet" type="text/css" href="css/miestilo.css">
+   </head>
+   <body class="pink lighten-4">
+   <div class="navbar-fixed">
+      <nav class="pink darken-1" role="navigation">
+         <div class="nav-wrapper container">
+            <a id="logo-container" href="index.html" class="brand-logo">AccesModa</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+            <ul class="right hide-on-med-and-down">
+                  <li><a href="ofertas.html">Ofertas</a></li>
+                  <li><a href="contacto.html">Contacto</a></li>
+              </ul>
+      
+              <ul class="side-nav" id="mobile-demo">
+                  <li><a href="ofertas.html">Ofertas</a></li>
+                  <li><a href="contacto.html">Contacto</a></li>
+               </ul>
+              </div>
+          </nav>
+      </div>
+      <br>
+      <br>
+        <!--Boton flotante-->
+    <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+        <a class="btn-floating btn-large red" href="index.html">
+          <i class="large mdi-action-home"></i>
+        </a>
+        <ul>
+          <li><a class="btn-floating green" href="registro.php"><i class="large mdi-social-person-add"></i></a></li>
+          <li><a class="btn-floating blue" href="login.php"><i class="large mdi-action-perm-identity"></i></a></li>
+        </ul>
+    </div>
+      <!--Boton flotante-->
+
+    <!--Formulario-->
+    <div class="container">
+    <form>
+        <input type="text" name="nombre" placeholder="Tu Nombre" class="colorform"><br>
+        <input type="email" name="email" placeholder="Tu Email" class="colorform"><br>
+        <input type="password" name="password" placeholder="Escribe tu contraseña" class="colorform"><br>
+        <input type="password" name="password" placeholder="Repite tu contraseña" class="colorform"><br>
+         <button class="btn pink darken-1" type="submit" name="action">Registro
+    <i class="mdi-content-send right"></i>
+  </button>
+    </form>
+    </div>
+    <!--Formulario-->
+
+
+    </body>
+ </html>
